249c50483cafe3102e741edce57daabde2bd34d8
Benjamin Wilkins <wilkben@yahoo.ca>
2018-09-25 20:26:48 -0400

    Generate all possible postal codes

--- canada_parser.py
+++ canada_parser.py
@@ -1,9 +1,17 @@
-import requests
+import random
+import grequests
+import itertools
 import json
 import re
 
-payload = {'zip': 'K1G0H3', 'section': 'Beaver Colony', 'weekdays[]': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']}
-r = requests.post("https://www.myscouts.ca/ca/netforum_locator/search", data = payload)
+payload = {'section': 'Scout Troop', 'weekdays[]': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']}
+sections = ['Beaver Colony', 'Cub Pack', 'Scout Troop', 'Venturer Company', 'Rover Crew']
+digit = '0123456789'
+postal_letter = 'ABCEGHJKLMNPRSTVWXYZ'
+postal_start = 'ABCEGHJKLMNPRSTVXY'
+url = "https://www.myscouts.ca/ca/netforum_locator/search"
+postal_codes = list(map(''.join, itertools.product(postal_start, digit, postal_letter, digit, postal_letter, digit)))
+data = set()
 
 data = re.findall("\"latitude\": \"(-?[0-9]*\.[0-9]*)\"\, \"longitude\": \"(-?[0-9]*\.[0-9]*)\"", r.text)
 
