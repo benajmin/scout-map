07e90673d7e5a75640ac03e213a4054ecdaae8bd
Benjamin Wilkins <wilkben@yahoo.ca>
2018-10-14 18:31:51 -0400

    Scrape data about canadian scout groups

--- canada_parser.py
+++ canada_parser.py
@@ -1,22 +1,35 @@
 import random
 import grequests
+import atexit
 import itertools
 import json
 import re
 
 payload = {'section': 'Scout Troop', 'weekdays[]': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']}
 sections = ['Beaver Colony', 'Cub Pack', 'Scout Troop', 'Venturer Company', 'Rover Crew']
-digit = '0123456789'
-postal_letter = 'ABCEGHJKLMNPRSTVWXYZ'
-postal_start = 'ABCEGHJKLMNPRSTVXY'
 url = "https://www.myscouts.ca/ca/netforum_locator/search"
-postal_codes = list(map(''.join, itertools.product(postal_start, digit, postal_letter, digit, postal_letter, digit)))
 data = set()
+f  = open('uniq_postal_codes.txt')
+postal_codes = f.readlines()
+f.close()
+err = open('err.txt', 'a')
 
-rs = (grequests.post(url, data=dict(payload, **{'zip': pc})) for pc in random.sample(postal_codes, 100))
-for response in grequests.map(rs):
-  data.update(re.findall("\"latitude\": \"(-?[0-9]*\.[0-9]*)\"\, \"longitude\": \"(-?[0-9]*\.[0-9]*)\"", response.text))
+def exit_handler():
+  f = open('canada_groups.txt', 'a')
+  for i in data:
+    f.write(i + '\n')
+  
+atexit.register(exit_handler)
 
+for i in range(710625, len(postal_codes), 75):
+  rs = (grequests.post(url, data=dict(payload, **{'zip': pc})) for pc in postal_codes[i:i+75])
+  for idx, response in enumerate(grequests.map(rs)):
+    if not response:
+      print("Error: no response: " + postal_codes[i+idx])
+      err.write(postal_codes[i+idx] + '\n')
+    else:
+      print(i)
+      data.update(re.findall("\"latitude\": \"-?[0-9]*\.[0-9]*\"\, \"longitude\": \"-?[0-9]*\.[0-9]*\"[^}]*}", response.text))
+      data.update(re.findall("<br\/>[^<]*<br\/>Meeting Day: [a-zA-Z]*<br\/>Meeting Time: [0-9]+:[0-9][0-9] [AP]M<br\/>Meeting Location: [^<]*<br\/>Registration Fee: \$[0-9]*<br \/>(?:<img src=\"\/sites\/all\/themes\/scouts\/images\/sunshine10x10\.png\" style=\"vertical-align:-15\%\">Program runs throughout the summer\.)?<", response.text))
+      response.close()
 
-print(data)
-print(len(data))  
