dea85dbf78137df9b100e0bb9e48d3baf48841e9
Benjamin Wilkins <wilkben@yahoo.ca>
2018-10-24 21:08:12 -0400

    Load heat map

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 var map;
-function initMap() {
+var heatmap;
+
+$.getJSON("canada.json", function(data) {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 60, lng: -106},
     zoom: 4
   });
-}
+
+  var items = [];
+  $.each( data, function( key, group ) {
+    items.push(new google.maps.LatLng(parseFloat(group['Latitude']), parseFloat(group['Longitude'])));
+  });
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: items,
+    map: map
+  })
+ });
+
