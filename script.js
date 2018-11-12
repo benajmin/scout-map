88824d87f0bd4e1e505b8d551110e62d260ad232
Benjamin Wilkins <wilkben@yahoo.ca>
2018-11-11 19:01:46 -0500

    Add info windows for each group

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var map;
 var heatmap;
 
+function displayInfoWindow(infowindow, map, marker) {
+  return function () {
+    infowindow.open(map, marker);
+  }
+}
+
 $.getJSON("canada.json", function(data) {
   hmap = new google.maps.Map(document.getElementById('heatmap'), {
     center: {lat: 60, lng: -106},
@@ -13,17 +19,33 @@ $.getJSON("canada.json", function(data) {
 
   var items = [];
   var markers = []
+  var infowindows = []
+  var contentstrings = []
   $.each( data, function( key, group ) {
     items.push(new google.maps.LatLng(parseFloat(group['Latitude']), parseFloat(group['Longitude'])));
-    markers.push(new google.maps.Marker({
+    contentstrings.push('<div id="content">' +
+      '<h4>' + group['Name'] + '</h4>' +
+      'Meets ' + group['Meeting Day'] + 's at ' + group['Meeting Time'] + ' at ' + group['Meeting Location'] + '<br/>' +
+      'Registration Fee: ' + group['Registration Fee'] + '<br/>' +
+      (group['Summer Program']? 'Runs Summer Program <br/>' : '') +
+      '</div>'
+    );
+
+    infowindows.push(new google.maps.InfoWindow({
+      content: contentstrings[contentstrings.length-1]
+    }));
+    var marker = new google.maps.Marker({
       position: items[items.length - 1],
       map: map,
-      title: group['Name']}));
+      title: group['Name']
+    });
+    marker.addListener('click', displayInfoWindow(infowindows[infowindows.length-1], map, marker));
+    markers.push(marker)
   });
 
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: items,
     map: hmap
   })
- });
+});
 
